8a466afe96f6eac88dfcc20ddf829a72b45f4932
Brian Herbst <bherbst@INV-3772.local>
2020-03-27 11:43:08 -0600

    added version 0.0.1

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
     $scope.loading = true;
-
+    $scope.version = "0.0.1";
     // Your web app's Firebase configuration
     var firebaseConfig = JSON.parse(fbcfg);
 
