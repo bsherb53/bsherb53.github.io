3824674e6eac42c86d4d083cf39a77a560118fde
Bee Herbst <bherbst@solutionreach.com>
2020-04-03 15:05:22 -0600

    minor update

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
     $scope.loading = true;
-    $scope.version = "0.1.8";
+    $scope.version = "0.2.0";
     $scope.admin = false;
     $scope.selected = undefined;
     $scope.select = function (n) {
