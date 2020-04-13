3d93a9200610f62c1aa3cfad58b516d678e089d1
Bee Herbst <bherbst@solutionreach.com>
2020-04-13 16:05:25 -0600

    more updates

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
     $scope.loading = true;
-    $scope.version = "0.3.21";
+    $scope.version = "0.3.22";
     $scope.admin = true;
     $scope.selected = undefined;
     var mapSpreadsheetID = '1B16F1-Dd4lGoAMhGfGTCRUl4FFQg9hBPsxYBXEJp9zI';
@@ -58,12 +58,13 @@ app.controller('myCtrl', function ($scope) {
             scope: SCOPES
         }).then(function () {
             if ($scope.admin) {
+                updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
                 gapi.auth2.getAuthInstance().signIn();
                 gapi.auth2.getAuthInstance().isSignedIn.listen(function (isSignedIn) {
                     if (isSignedIn) {
                         authorizeButton.style.display = 'none';
                         signoutButton.style.display = 'block';
-                        listMajors();
                     } else {
                         authorizeButton.style.display = 'block';
                         signoutButton.style.display = 'none';
