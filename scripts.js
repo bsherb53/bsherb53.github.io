b2e3184a1ef6d95c5d007627b8729b7a8bc5bf8b
Bee Herbst <bherbst@solutionreach.com>
2020-04-13 10:25:35 -0600

    more updates

--- scripts.js
+++ scripts.js
@@ -2,9 +2,10 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
     $scope.loading = true;
-    $scope.version = "0.2.7";
+    $scope.version = "0.2.8";
     $scope.admin = true;
     $scope.selected = undefined;
+    var mapSpreadsheetID = '1B16F1-Dd4lGoAMhGfGTCRUl4FFQg9hBPsxYBXEJp9zI';
     $scope.select = function (n) {
         $scope.old = JSON.parse(JSON.stringify($scope.tiles[n]));
         console.log("selected ", n, $scope.tiles[n])
@@ -19,56 +20,6 @@ app.controller('myCtrl', function ($scope) {
         $scope.selected = undefined;
     };
 
-    var save = function (t) {
-        // console.log("implement saving ", t, $scope.old)
-        var old = $scope.old;
-        console.log("old", old)
-        // console.log("new", t)
-        var different = old.number != t.number || old.name != t.name ||
-            old.biome != t.biome || old.type != t.type ||
-            old.weather != t.weather || old.rating != t.rating ||
-            old.owner.name != t.owner.name;
-        if (!different) {
-            // console.log("items not different");
-            return
-        }
-
-
-        console.log("saving");
-
-        // if (t.id == undefined) {
-        //     console.log("undefined id", t);
-        //     return;
-        // }
-        // // var temp = $scope.tiles;
-        // $scope.tiles[t.number] = t;
-        // // $scope.tiles = temp;
-        // // save to cache too: 
-        // localStorage['tiles'] = JSON.stringify($scope.tiles);
-
-        // // Get a new write batch
-        // var batch = db.batch();
-
-        // // Update the population of 'SF'
-        // var sfRef = db.collection("tiles").doc(t.id);
-        // batch.update(sfRef, {
-        //     number: t.number,
-        //     name: t.name,
-        //     biome: t.biome,
-        //     type: t.type,
-        //     weather: t.weather,
-        //     rating: t.rating,
-        //     owner: t.owner
-        // });
-
-        // // Commit the batch
-        // batch.commit().then(function () {
-        //     // ...
-        //     console.log("saved", t);
-        //     // console.log("batch complete");
-        // });
-
-    }
     var API_KEY = 'AIzaSyC8fZMlxqKFkt5Wu0sLZUhzXFhuUfa0ZpQ';
     var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 
@@ -101,10 +52,11 @@ app.controller('myCtrl', function ($scope) {
         $scope.tiles = [];
         console.log("loading tiles");
         gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: '1B16F1-Dd4lGoAMhGfGTCRUl4FFQg9hBPsxYBXEJp9zI',
+            spreadsheetId: mapSpreadsheetID,
             range: 'MapData!A:J',
         }).then(function (response) {
             var range = response.result;
+            console.log("range", range);
             if (range.values.length > 0) {
                 for (i = 1; i < range.values.length; i++) { // i = 1 to ignore the header row
                     var row = range.values[i];
@@ -204,6 +156,57 @@ app.controller('myCtrl', function ($scope) {
 
     }
 
+    var save = function (t) {
+        // console.log("implement saving ", t, $scope.old)
+        var old = $scope.old;
+        console.log("old", old)
+        // console.log("new", t)
+        var different = old.number != t.number || old.name != t.name ||
+            old.biome != t.biome || old.type != t.type ||
+            old.weather != t.weather || old.rating != t.rating ||
+            old.owner.name != t.owner.name;
+        if (!different) {
+            // console.log("items not different");
+            return
+        }
+
+
+        console.log("saving");
+        //  {
+        //     number: t.number,
+        //     name: t.name,
+        //     biome: t.biome,
+        //     type: t.type,
+        //     weather: t.weather,
+        //     rating: t.rating,
+        //     owner: t.owner
+        // }
+
+
+        // var params = {
+        //     // The ID of the spreadsheet to update.
+        //     spreadsheetId: mapSpreadsheetID,  // TODO: Update placeholder value.
+    
+        //     // The A1 notation of the values to update.
+        //     range: 'my-range',  // TODO: Update placeholder value.
+    
+        //     // How the input data should be interpreted.
+        //     valueInputOption: '',  // TODO: Update placeholder value.
+        //   };
+    
+        //   var valueRangeBody = {
+        //     // TODO: Add desired properties to the request body. All existing properties
+        //     // will be replaced.
+        //   };
+        //   var request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
+        //   request.then(function(response) {
+        //     // TODO: Change code below to process the `response` object:
+        //     console.log(response.result);
+        //   }, function(reason) {
+        //     console.error('error: ' + reason.result.error.message);
+        //   });
+
+    }
     // handleClientLoad();
 });
 
