22c158129684642c015aee1ce6fa92fa656a2988
Bee Herbst <bherbst@solutionreach.com>
2020-04-14 15:59:48 -0600

    fixed row that is being updated

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
     $scope.loading = true;
-    $scope.version = "0.3.45";
+    $scope.version = "0.3.46";
     $scope.admin = true;
     $scope.selected = undefined;
     var mapSpreadsheetID = '1B16F1-Dd4lGoAMhGfGTCRUl4FFQg9hBPsxYBXEJp9zI';
@@ -78,7 +78,7 @@ app.controller('myCtrl', function ($scope) {
                         rating: row[5],
                         type: row[6],
                         weather: row[7],
-                        row: i,
+                        row: i+1,
                     }
 
                     $scope.tiles.push(tile);
