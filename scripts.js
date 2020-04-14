cb88de1dc73506e584305dc82359125c8f223287
Bee Herbst <bherbst@solutionreach.com>
2020-04-14 15:33:40 -0600

    updated stuff

--- scripts.js
+++ scripts.js
@@ -267,12 +267,19 @@ app.controller('myCtrl', function ($scope) {
         });
     }
 
-    $scope.onSignIn = function(googleUser) {
+    $scope.onSignIn = function (googleUser) {
         var profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      }
+    }
+
+    $scope.signOut = function() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
+    }
 });
 
