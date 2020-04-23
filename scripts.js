5edab46e45c7cc78f50b17b2173a41149c9ef1fe
Bee Herbst <bherbst@solutionreach.com>
2020-04-23 11:50:56 -0600

    master: stuff

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
     $scope.loading = true;
-    $scope.version = "1.0.10";
+    $scope.version = "1.0.11";
     $scope.admin = false;
     $scope.selected = undefined;
     $scope.biomes = [];
