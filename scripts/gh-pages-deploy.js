fb39893ee6649f0099f3ae124d3922d89bd8d3b6
Ginger King <briansherbst@gmail.com>
2023-12-14 22:37:51 -0700

    added console logs

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -7,21 +7,31 @@ const secondLog = `${emoji.get('fast_forward')} ${chalk.yellow('Pushing...')}`
 const thirdLog = `${emoji.get('rocket')} ${chalk.green('Your app successfully deployed')} ${emoji.get('rocket')}`
 
 ;(async () => {
-  try {
-    const { stdout: currentBranch } = await execa.command('git branch --show-current')
-    await execa.execaCommand('git checkout --orphan gh-page')
-    console.log(firstLog)
-    await execa.execaCommand('npm run build', { stdio: 'inherit' })
-    await execa.execaCommand('git --work-tree dist add --all')
-    await execa.execaCommand('git --work-tree dist commit -m "gh-page"')
-    console.log(secondLog)
-    await execa.execaCommand('git push origin HEAD:gh-page --force', { stdio: 'inherit' })
-    await execa.execaCommand('rm -r dist')
-    await execa.execaCommand(`git checkout -f ${currentBranch}`)
-    await execa.execaCommand('git branch -D gh-page')
-    console.log(thirdLog)
-  } catch (e) {
-    console.log(e.message)
-    process.exit(1)
-  }
+    try {
+        console.log('b1');
+        const {stdout: currentBranch} = await execa.command('git branch --show-current')
+        console.log('b2');
+        await execa.execaCommand('git checkout --orphan gh-page')
+        console.log('b3');
+        console.log(firstLog)
+        await execa.execaCommand('npm run build', {stdio: 'inherit'})
+        console.log('b1');
+        await execa.execaCommand('git --work-tree dist add --all')
+        console.log('b1');
+        await execa.execaCommand('git --work-tree dist commit -m "gh-page"')
+        console.log('b1');
+        console.log(secondLog)
+        await execa.execaCommand('git push origin HEAD:gh-page --force', {stdio: 'inherit'})
+        console.log('b1');
+        await execa.execaCommand('rm -r dist')
+        console.log('b1');
+        await execa.execaCommand(`git checkout -f ${currentBranch}`)
+        console.log('b1');
+        await execa.execaCommand('git branch -D gh-page')
+        console.log('b1');
+        console.log(thirdLog)
+    } catch (e) {
+        console.log(e.message)
+        process.exit(1)
+    }
 })()
