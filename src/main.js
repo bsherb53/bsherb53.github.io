f1c57b9eef1eb9a0ffcd32c9c7202492defce415
Ginger King <briansherbst@gmail.com>
2023-12-14 23:15:03 -0700

    rebuilt dist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import MinuteHero from "@/components/minuteHero/MinuteHero";
