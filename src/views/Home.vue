0c6d66b1f5df5e8c716cdbc21ea3f502b5d9aa1e
Barbie <briansherbst@gmail.com>
2025-10-24 23:18:41 -0600

    added spotify playlist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Welcome To Red Hollow</div>
+  <div style="display: flex; flex-direction: column; margin: auto; justify-content: center">
+    <div>Welcome To Red Hollow</div>
+    <div class="wrapper">
+    <iframe data-testid="embed-iframe" style="border-radius:12px"
+            src="https://open.spotify.com/embed/playlist/0FdQ6y4CoJT68BtjOwIcKl?utm_source=generator&theme=0"
+            width="100%" height="100%" frameBorder="0" allowfullscreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -8,5 +16,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.wrapper{
+  margin: auto;
+  width: 80%;
+  height: 75vh;
+}
 </style>
